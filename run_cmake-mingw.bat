a5a889cfc28e75e4fff0f292fe8c32631c73af27
Rene Barto <rene.barto66@gmail.com>
2018-10-31 01:02:16 +0100

    Added OSAL::ByteArray implementation and tests.

--- run_cmake-mingw.bat
+++ run_cmake-mingw.bat
@@ -1,5 +1,7 @@
+setlocal
 mkdir cmake-mingw-debug
 cd cmake-mingw-debug
 del CMakeCache.txt
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DSCRIPTS_DIR="F:/My_Projects/cmake-scripts" -DCMAKE_INSTALL_PREFIX="F:\My_Projects\install-mingw" -DPLATFORM_INSTALL="F:\My_Projects\install-mingw" -G "MinGW Makefiles"
 cd ..
+endlocal
--- source/components/osal/export/osal/ByteArray.h
+++ source/components/osal/export/osal/ByteArray.h
@@ -9,15 +9,15 @@
 namespace OSAL
 {
 
-class OSAL_EXPORT ByteArray : public Array<uint8_t>
+class ByteArray : public Array<uint8_t>
 {
 public:
     ByteArray() : Array() {}
     ByteArray(size_t size) : Array(size) {}
     ByteArray(const uint8_t * data, size_t length) : Array(data, length) {}
-    ByteArray(const char * data, size_t length);
-    explicit ByteArray(const char * data);
-    ByteArray(std::string data);
+    OSAL_EXPORT ByteArray(const char * data, size_t length);
+    explicit OSAL_EXPORT ByteArray(const char * data);
+    OSAL_EXPORT ByteArray(std::string data);
     ByteArray(const ByteArray & other) : Array(other) {}
     ByteArray(ByteArray && other) : Array()
     {
@@ -25,13 +25,13 @@ public:
     }
     ByteArray(std::initializer_list<uint8_t> data) : Array(data) {}
 
-    uint8_t GetUInt8(size_t offset) const;
-    uint16_t GetUInt16(size_t offset) const;
-    uint32_t GetUInt32(size_t offset) const;
-    uint64_t GetUInt64(size_t offset) const;
-    uint16_t GetUInt16BE(size_t offset) const;
-    uint32_t GetUInt32BE(size_t offset) const;
-    uint64_t GetUInt64BE(size_t offset) const;
+    OSAL_EXPORT uint8_t GetUInt8(size_t offset) const;
+    OSAL_EXPORT uint16_t GetUInt16(size_t offset) const;
+    OSAL_EXPORT uint32_t GetUInt32(size_t offset) const;
+    OSAL_EXPORT uint64_t GetUInt64(size_t offset) const;
+    OSAL_EXPORT uint16_t GetUInt16BE(size_t offset) const;
+    OSAL_EXPORT uint32_t GetUInt32BE(size_t offset) const;
+    OSAL_EXPORT uint64_t GetUInt64BE(size_t offset) const;
     size_t Get(size_t offset, uint8_t * data, size_t length) const
     {
         return Array::Get(offset, data, length);
@@ -40,21 +40,21 @@ public:
     {
         return Array::Get(offset, data, length);
     }
-    ByteArray Get(size_t offset, size_t length) const;
-    void SetUInt8(size_t offset, uint8_t value);
-    void SetUInt16(size_t offset, uint16_t value);
-    void SetUInt32(size_t offset, uint32_t value);
-    void SetUInt64(size_t offset, uint64_t value);
-    void SetUInt16BE(size_t offset, uint16_t value);
-    void SetUInt32BE(size_t offset, uint32_t value);
-    void SetUInt64BE(size_t offset, uint64_t value);
+    OSAL_EXPORT ByteArray Get(size_t offset, size_t length) const;
+    OSAL_EXPORT void SetUInt8(size_t offset, uint8_t value);
+    OSAL_EXPORT void SetUInt16(size_t offset, uint16_t value);
+    OSAL_EXPORT void SetUInt32(size_t offset, uint32_t value);
+    OSAL_EXPORT void SetUInt64(size_t offset, uint64_t value);
+    OSAL_EXPORT void SetUInt16BE(size_t offset, uint16_t value);
+    OSAL_EXPORT void SetUInt32BE(size_t offset, uint32_t value);
+    OSAL_EXPORT void SetUInt64BE(size_t offset, uint64_t value);
     void Append(const ByteArray & data)
     {
         Set(this->Size(), data);
     }
-    ByteArray & operator = (const ByteArray & other);
-    ByteArray & operator = (ByteArray && other);
-    std::ostream & PrintTo(std::ostream & stream) const override;
+    OSAL_EXPORT ByteArray & operator = (const ByteArray & other);
+    OSAL_EXPORT ByteArray & operator = (ByteArray && other);
+    OSAL_EXPORT std::ostream & PrintTo(std::ostream & stream) const override;
 };
 
 inline void PrintTo(const ByteArray & value, std::ostream & stream)
