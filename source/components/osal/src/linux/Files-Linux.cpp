cca5a4955e3eb2b86f371f5b8cce562adb36d8d6
Rene Barto <rene.barto66@gmail.com>
2018-11-23 00:07:27 +0100

    Fix build for Linux

--- source/components/osal/src/linux/Files-Linux.cpp
+++ source/components/osal/src/linux/Files-Linux.cpp
@@ -47,123 +47,123 @@ std::string const & TemporaryFile::GetPath() const
     return _path;
 }
 
-FileList Files::ScanForFiles(const std::string & searchPattern,
-                             SearchOption searchOption)
-{
-    FileInfo::List result;
-
-    if (!GetFilesInCurrentDirectory(result, FullPath(), searchPattern, searchOption))
-        return {};
-
-    string wildcardSpecifier = OSAL::Path::CombinePath(FullPath(), "*");
-    glob_t globBuffer;
-
-    DirectoryInfo::List subDirectories;
-
-    glob(wildcardSpecifier.c_str(), GLOB_PERIOD | GLOB_ONLYDIR, nullptr, &globBuffer);
-
-    for (size_t i = 0; i < globBuffer.gl_pathc; i++)
-    {
-        string directoryName = OSAL::Path::LastPartOfPath(globBuffer.gl_pathv[i]);
-        if ((directoryName == ".") || (directoryName == ".."))
-            continue;
-        Directory directory(OSAL::Path::FullPath(globBuffer.gl_pathv[i]));
-
-        if (!HaveDirectory(subDirectories, directory.Name()))
-            AddDirectory(subDirectories, directory);
-        if ((searchOption & SearchOption::Recursive) != 0)
-        {
-            FileInfo::List subDirectoryResult = directory.ScanForFiles(searchPattern, searchOption);
-            result.insert(result.end(), subDirectoryResult.begin(), subDirectoryResult.end());
-        }
-    }
-    globfree(&globBuffer);
-
-    return result;
-}
-
-FileList Files::ScanForDirectories(const std::string & searchPattern,
-                                   SearchOption searchOption)
-{
-    DirectoryInfo::List result;
-
-    string wildcardSpecifier = OSAL::Path::CombinePath(FullPath(), searchPattern);
-    glob_t globBuffer;
-
-    DirectoryInfo::List subDirectories;
-
-    glob(wildcardSpecifier.c_str(), GLOB_PERIOD | GLOB_ONLYDIR, nullptr, &globBuffer);
-
-    for (size_t i = 0; i < globBuffer.gl_pathc; i++)
-    {
-        string directoryName = OSAL::Path::LastPartOfPath(globBuffer.gl_pathv[i]);
-        if ((directoryName == ".") || (directoryName == ".."))
-            continue;
-        string directoryPath = globBuffer.gl_pathv[i];
-        if ((searchOption & SearchOption::IncludeSymbolicLinks) == 0)
-        {
-            // Resolve symbolic link
-            directoryPath = OSAL::Path::FullPath(directoryPath);
-        }
-        else
-        {
-            DirectoryInfo tmp(OSAL::Path::FullPath(directoryPath));
-            if (!tmp.IsDirectory())
-                continue;
-        }
-        Directory directory(directoryPath);
-        if (!HaveDirectory(result, directory.FullPath()) &&
-            (((searchOption & SearchOption::IncludeHidden) != 0) ||
-            !directory.IsHidden()))
-            AddDirectory(result, directory);
-
-        if (!HaveDirectory(subDirectories, directory.Name()))
-            AddDirectory(subDirectories, directory);
-        if ((searchOption & SearchOption::Recursive) != 0)
-        {
-            DirectoryInfo::List subDirectoryResult = directory.ScanForDirectories(searchPattern, searchOption);
-            result.insert(result.end(), subDirectoryResult.begin(), subDirectoryResult.end());
-        }
-    }
-    globfree(&globBuffer);
-
-    return result;
-}
-
-FileList Files::GetFilesInCurrentDirectory(const std::string & path,
-                                           const std::string & searchPattern,
-                                           SearchOption searchOption)
-{
-    string wildcardSpecifier = OSAL::Path::CombinePath(path, searchPattern);
-    glob_t globBuffer;
-
-    glob(wildcardSpecifier.c_str(), GLOB_PERIOD, nullptr, &globBuffer);
-
-    for (size_t i = 0; i < globBuffer.gl_pathc; i++)
-    {
-        string filePath = globBuffer.gl_pathv[i];
-        if ((searchOption & SearchOption::IncludeSymbolicLinks) == 0)
-        {
-            // Resolve symbolic link
-            filePath = OSAL::Path::FullPath(filePath);
-        }
-        else
-        {
-            FileInfo tmp(OSAL::Path::FullPath(filePath));
-            if (!tmp.IsRegularFile())
-                continue;
-        }
-        FileInfo fileInfo(filePath);
-        if (!fileInfo.IsDirectory() &&
-            (((searchOption & SearchOption::IncludeHidden) != 0) ||
-             !fileInfo.IsHidden()))
-        {
-            if (!HaveFile(result, fileInfo.FullPath()))
-                AddFile(result, fileInfo);
-        }
-    }
-    globfree(&globBuffer);
-    return true;
-}
+//FileList Files::ScanForFiles(const std::string & searchPattern,
+//                             SearchOption searchOption)
+//{
+//    FileInfo::List result;
+//
+//    if (!GetFilesInCurrentDirectory(result, FullPath(), searchPattern, searchOption))
+//        return {};
+//
+//    string wildcardSpecifier = OSAL::Path::CombinePath(FullPath(), "*");
+//    glob_t globBuffer;
+//
+//    DirectoryInfo::List subDirectories;
+//
+//    glob(wildcardSpecifier.c_str(), GLOB_PERIOD | GLOB_ONLYDIR, nullptr, &globBuffer);
+//
+//    for (size_t i = 0; i < globBuffer.gl_pathc; i++)
+//    {
+//        string directoryName = OSAL::Path::LastPartOfPath(globBuffer.gl_pathv[i]);
+//        if ((directoryName == ".") || (directoryName == ".."))
+//            continue;
+//        Directory directory(OSAL::Path::FullPath(globBuffer.gl_pathv[i]));
+//
+//        if (!HaveDirectory(subDirectories, directory.Name()))
+//            AddDirectory(subDirectories, directory);
+//        if ((searchOption & SearchOption::Recursive) != 0)
+//        {
+//            FileInfo::List subDirectoryResult = directory.ScanForFiles(searchPattern, searchOption);
+//            result.insert(result.end(), subDirectoryResult.begin(), subDirectoryResult.end());
+//        }
+//    }
+//    globfree(&globBuffer);
+//
+//    return result;
+//}
+//
+//FileList Files::ScanForDirectories(const std::string & searchPattern,
+//                                   SearchOption searchOption)
+//{
+//    DirectoryInfo::List result;
+//
+//    string wildcardSpecifier = OSAL::Path::CombinePath(FullPath(), searchPattern);
+//    glob_t globBuffer;
+//
+//    DirectoryInfo::List subDirectories;
+//
+//    glob(wildcardSpecifier.c_str(), GLOB_PERIOD | GLOB_ONLYDIR, nullptr, &globBuffer);
+//
+//    for (size_t i = 0; i < globBuffer.gl_pathc; i++)
+//    {
+//        string directoryName = OSAL::Path::LastPartOfPath(globBuffer.gl_pathv[i]);
+//        if ((directoryName == ".") || (directoryName == ".."))
+//            continue;
+//        string directoryPath = globBuffer.gl_pathv[i];
+//        if ((searchOption & SearchOption::IncludeSymbolicLinks) == 0)
+//        {
+//            // Resolve symbolic link
+//            directoryPath = OSAL::Path::FullPath(directoryPath);
+//        }
+//        else
+//        {
+//            DirectoryInfo tmp(OSAL::Path::FullPath(directoryPath));
+//            if (!tmp.IsDirectory())
+//                continue;
+//        }
+//        Directory directory(directoryPath);
+//        if (!HaveDirectory(result, directory.FullPath()) &&
+//            (((searchOption & SearchOption::IncludeHidden) != 0) ||
+//            !directory.IsHidden()))
+//            AddDirectory(result, directory);
+//
+//        if (!HaveDirectory(subDirectories, directory.Name()))
+//            AddDirectory(subDirectories, directory);
+//        if ((searchOption & SearchOption::Recursive) != 0)
+//        {
+//            DirectoryInfo::List subDirectoryResult = directory.ScanForDirectories(searchPattern, searchOption);
+//            result.insert(result.end(), subDirectoryResult.begin(), subDirectoryResult.end());
+//        }
+//    }
+//    globfree(&globBuffer);
+//
+//    return result;
+//}
+//
+//FileList Files::GetFilesInCurrentDirectory(const std::string & path,
+//                                           const std::string & searchPattern,
+//                                           SearchOption searchOption)
+//{
+//    string wildcardSpecifier = OSAL::Path::CombinePath(path, searchPattern);
+//    glob_t globBuffer;
+//
+//    glob(wildcardSpecifier.c_str(), GLOB_PERIOD, nullptr, &globBuffer);
+//
+//    for (size_t i = 0; i < globBuffer.gl_pathc; i++)
+//    {
+//        string filePath = globBuffer.gl_pathv[i];
+//        if ((searchOption & SearchOption::IncludeSymbolicLinks) == 0)
+//        {
+//            // Resolve symbolic link
+//            filePath = OSAL::Path::FullPath(filePath);
+//        }
+//        else
+//        {
+//            FileInfo tmp(OSAL::Path::FullPath(filePath));
+//            if (!tmp.IsRegularFile())
+//                continue;
+//        }
+//        FileInfo fileInfo(filePath);
+//        if (!fileInfo.IsDirectory() &&
+//            (((searchOption & SearchOption::IncludeHidden) != 0) ||
+//             !fileInfo.IsHidden()))
+//        {
+//            if (!HaveFile(result, fileInfo.FullPath()))
+//                AddFile(result, fileInfo);
+//        }
+//    }
+//    globfree(&globBuffer);
+//    return true;
+//}
 
 #endif // defined(LINUX)
